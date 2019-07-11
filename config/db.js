18e8657f5d6f5c9163b2067c1e000a8a97061c5f
Inaldo Souza <inaldoss@hotmail.com>
2019-07-11 13:29:43 -0300

    correcao mongoDB Flavio

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV == "production") {
-    module.exports = { MONGOURI: "mongodb+srv://inaldoss@hotmail.com:scastro126@blogapp-prod-s7dn6.mongodb.net/test?retryWrites=true&w=majority" }
+    module.exports = { MONGOURI: "mongodb+srv://inaldoss:scastro126@blogapp-prod-s7dn6.mongodb.net/authSource=admin" }
 } else {
+    //module.exports = { MONGOURI: "mongodb+srv://inaldoss:scastro126@blogapp-prod-s7dn6.mongodb.net/authSource=admin" }
     module.exports = { MONGOURI: "mongodb://localhost/blogapp" }
 }
