6ac6611f72b389e1b2625d4b82aa6bb9dc057dfe
Inaldo Souza <inaldoss@hotmail.com>
2019-07-11 09:30:19 -0300

    Correcao para corrigir conexao com o mongoDB

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-    module.exports = { MONGOURI: "mongodb+srv://inaldoss:scastro126@blogapp-prod-s7dn6.mongodb.net/test?retryWrites=true&w=majority" }
+    module.exports = { MONGOURI: "mongodb+srv://inaldoss@hotmail.com:scastro126@blogapp-prod-s7dn6.mongodb.net/test?retryWrites=true&w=majority" }
 } else {
     module.exports = { MONGOURI: "mongodb://localhost/blogapp" }
 }
